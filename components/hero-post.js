b2b0db646cda1df0065d30a30d7205120da423fd
LiuuY <mail4c59@gmail.com>
2020-05-06 18:28:47 +0800

    font

--- components/hero-post.js
+++ components/hero-post.js
@@ -11,14 +11,14 @@ export default function HeroPost({
 }) {
   return (
     <section>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-6 md:mb-8">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+          <h3 className="mb-2 text-base lg:text-base leading-tight">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
+          <div className="mb-4 md:mb-0 text-sm text-gray-500">
             <DateFormater dateString={date} />
           </div>
         </div>
--- components/intro.js
+++ components/intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <div className="flex flex-row items-center flex-no-wrap divide-x">
+      <div className="flex flex-row items-center flex-no-wrap divide-x mt-4">
         <h4 className="text-center md:text-left text-sm px-2">我</h4>
         <h4 className="text-center md:text-left text-sm px-2">刘宇</h4>
         <h4 className="text-center md:text-left text-sm px-2">LiuuY</h4>
