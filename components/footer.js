0183641ace56c189f48069d9efd58433a4042623
LiuuY <mail4c59@gmail.com>
2020-05-06 18:28:14 +0800

    footer img

--- components/footer.js
+++ components/footer.js
@@ -2,15 +2,13 @@ import Container from "./container";
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="">
       <Container>
-        <div className="py-10 flex flex-col lg:flex-row items-center">
-          <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-            <img alt="知识共享许可协议" src="https://i.creativecommons.org/l/by/4.0/80x15.png" />
+        <img className="w-full" src="/assets/me/home.jpeg"></img>
+        <div className="py-2 flex flex-col lg:flex-row items-center">
+          <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">
+            CC BY-NC 4.0 
           </a>
-          本作品采用
-          <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">知识共享署名 4.0 国际许可协议</a>
-          进行许可。
         </div>
       </Container>
     </footer>
