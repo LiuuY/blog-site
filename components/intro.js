91c325ab7bdabee3c371967da86f753fca8005bb
LiuuY <mail4c59@gmail.com>
2020-05-06 14:42:00 +0800

    intro

--- components/intro.js
+++ components/intro.js
@@ -4,9 +4,17 @@ export default function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        我，刘宇， a.k.a. LiuuY
-      </h4>
+      <div className="flex flex-row items-center flex-no-wrap divide-x">
+        <h4 className="text-center md:text-left text-sm px-2">我</h4>
+        <h4 className="text-center md:text-left text-sm px-2">刘宇</h4>
+        <h4 className="text-center md:text-left text-sm px-2">LiuuY</h4>
+        <a href="mailto:mail4c59@gmail.com" target="_blank">
+          <img alt="mailto:mail4c59@gmail.com" src="assets/me/gmail.svg" className="w-4 mx-2" />
+        </a>
+        <a href="https://github.com/LiuuY" target="_blank">
+          <img alt="https://github.com/LiuuY" src="assets/me/github.svg" className="w-4 mx-2" />
+        </a>
+      </div>
     </section>
   )
 }
