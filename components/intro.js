5ffccd0366488fea496490903603393534354c20
LiuuY <mail4c59@gmail.com>
2020-05-06 12:58:56 +0800

    name

--- components/intro.js
+++ components/intro.js
@@ -4,6 +4,9 @@ export default function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+        我，刘宇， a.k.a. LiuuY
+      </h4>
     </section>
   )
 }
