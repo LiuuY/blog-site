f9ad48db85df807627b881e8cd399942d58b194d
LiuuY <mail4c59@gmail.com>
2020-05-07 15:18:19 +0800

    date coloe

--- components/post-header.js
+++ components/post-header.js
@@ -8,7 +8,9 @@ export default function PostHeader({ title, date, author }) {
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
         <Avatar name={author.name} picture={author.picture} />
-        <DateFormater dateString={date} />
+        <div className="text-gray-500">
+          <DateFormater dateString={date} />
+        </div>
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
