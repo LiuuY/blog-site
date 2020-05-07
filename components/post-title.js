7ffb6e60bd55dc9aec261cc8a7d12a51eacfafa2
LiuuY <mail4c59@gmail.com>
2020-05-07 15:51:16 +0800

    style

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none mb-8 text-center md:text-left">
       {children}
     </h1>
   )
