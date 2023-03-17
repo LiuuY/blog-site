26969ed86c8a82286d9237c77cf7ec9cfecfafaa
LiuuY <mail4c59@gmail.com>
2023-03-17 15:00:26 +0800

    upgrade to 13

--- components/hero-post.js
+++ components/hero-post.js
@@ -2,14 +2,18 @@ import Avatar from "../components/avatar";
 import DateFormater from "../components/date-formater";
 import Link from "next/link";
 
-export default function HeroPost({ title, date, excerpt, author, slug }) {
+export default function HeroPost({title, date, excerpt, author, slug}) {
   return (
     <section>
       <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-6 md:mb-8">
         <div>
           <h3 className="mb-1 text-base lg:text-base leading-tight tracking-wide">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a className="hover:underline">{title}</a>
+            <Link
+              as={`/posts/${slug}`}
+              href="/posts/[slug]"
+              className="hover:underline"
+            >
+              {title}
             </Link>
           </h3>
           <div className="mb-4 md:mb-0 text-xs text-gray-500">
--- components/post-preview.js
+++ components/post-preview.js
@@ -1,19 +1,17 @@
-import Avatar from '../components/avatar'
-import DateFormater from '../components/date-formater'
-import Link from 'next/link'
+import Avatar from "../components/avatar";
+import DateFormater from "../components/date-formater";
+import Link from "next/link";
 
-export default function PostPreview({
-  title,
-  date,
-  excerpt,
-  author,
-  slug,
-}) {
+export default function PostPreview({title, date, excerpt, author, slug}) {
   return (
     <div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+        <Link
+          as={`/posts/${slug}`}
+          href="/posts/[slug]"
+          className="hover:underline"
+        >
+          {title}
         </Link>
       </h3>
       <div className="text-lg mb-4">
@@ -22,5 +20,5 @@ export default function PostPreview({
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
       <Avatar name={author.name} picture={author.picture} />
     </div>
-  )
+  );
 }
