fb8494663c0544198bf3f70d798696189a667412
LiuuY <mail4c59@gmail.com>
2023-11-03 09:05:45 +0000

    date format

--- components/date-formater.js
+++ components/date-formater.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function DateFormater({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'MM/dd/yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'yyyy-MM-dd')}</time>
 }
